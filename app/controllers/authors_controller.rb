c1be00aeae00240a535932c6c1831c9b56fa28ef
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-07-21 20:38:35 -0500

    commit tree b09c238d525723fb2e1251783a449018b49b75bc

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,5 @@
 class AuthorsController < ApplicationController
-  
+
   def show
     author = Author.find(params[:id])
 
@@ -7,9 +7,10 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+    rescue ActiveRecord::RecordInvalid => invalid 
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
@@ -18,4 +19,5 @@ class AuthorsController < ApplicationController
     params.permit(:email, :name)
   end
   
+
 end
